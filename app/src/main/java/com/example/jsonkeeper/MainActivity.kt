caf9b7cc0ee60055c2c9bdcf394eebb0c9101849
LuzArelyMtz <luzarelymtz@hotmail.com>
2024-02-25 01:27:59 -0600

    Refactore Call for Coroutines

--- app/src/main/java/com/example/jsonkeeper/MainActivity.kt
+++ app/src/main/java/com/example/jsonkeeper/MainActivity.kt
@@ -3,33 +3,31 @@ package com.example.jsonkeeper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.example.jsonkeeper.api.JsonKeeperAPIImpl
-import com.example.jsonkeeper.api.model.JsonKeeperResponse
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.example.jsonkeeper.api.model.JsonKeeperItem
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     lateinit var tvJsonResponse: TextView
+
+    private var _livedataResponse = MutableLiveData<List<JsonKeeperItem>>()
+    var livedataResponse: LiveData<List<JsonKeeperItem>> = _livedataResponse
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         tvJsonResponse = findViewById(R.id.tvJsonResponse)
 
-        val call = JsonKeeperAPIImpl().getResponse()
-        call.enqueue(object : Callback<JsonKeeperResponse> {
-            override fun onResponse(
-                call: Call<JsonKeeperResponse>,
-                response: Response<JsonKeeperResponse>
-            ) {
-                tvJsonResponse.text = response.body().toString()
-            }
+        GlobalScope.launch {
+            _livedataResponse.postValue(JsonKeeperAPIImpl().getResponse().items)
+        }
 
-            override fun onFailure(call: Call<JsonKeeperResponse>, t: Throwable) {
-                println(t.printStackTrace().toString())
-            }
-
+        livedataResponse.observe(this, Observer { jsonKeeperList ->
+            tvJsonResponse.text = jsonKeeperList[0].toString()
         })
     }
 }
--- app/src/main/java/com/example/jsonkeeper/api/IJsonKeeperAPIClient.kt
+++ app/src/main/java/com/example/jsonkeeper/api/IJsonKeeperAPIClient.kt
@@ -1,10 +1,9 @@
 package com.example.jsonkeeper.api
 
 import com.example.jsonkeeper.api.model.JsonKeeperResponse
-import retrofit2.Call
 import retrofit2.http.GET
 
 interface IJsonKeeperAPIClient {
     @GET("/b/WN0G")
-    fun getResponse(): Call<JsonKeeperResponse>
+    suspend fun getResponse(): JsonKeeperResponse
 }
